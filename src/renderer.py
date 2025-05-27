30e8ad1069db7572eac41fef76b1e2fbe3955485
Soban Gianluca <gianluca.soban@isisbem.it>
2025-05-27 17:07:00 +0200

    v0.0.3 better

--- src/renderer.py
+++ src/renderer.py
@@ -71,25 +71,3 @@ class Renderer:
     
     def close(self):
         self.plotter.close()
-
-if __name__ == "__main__":
-    pygame.init()
-    screen = pygame.display.set_mode((800, 600))
-    pygame.display.set_caption("Rubik's Cube")
-    
-    renderer = Renderer()
-    clock = pygame.time.Clock()
-    
-    running = True
-    while running:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                running = False
-        
-        renderer.rotate_camera(1)  # Auto-rotate
-        screen.blit(renderer.render_frame(), (0, 0))
-        pygame.display.flip()
-        clock.tick(30)
-    
-    renderer.close()
-    pygame.quit()
